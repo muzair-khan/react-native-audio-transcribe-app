350436d7f3813645f0fcba1eb088df23ff0837fa
muzair-khan <dev.uzairkhan1486@gmail.com>
2024-11-14 04:48:19 -0600

    added new icons

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -1,6 +1,10 @@
 const icons = {
   usIcon: require('./us.png'),
   spainIcon: require('./spain.png'),
+  rewindIcon: require('./rewind.png'),
+  fastForwardIcon: require('./fast-forward.png'),
+  playIcon: require('./play.png'),
+  pauseIcon: require('./pause.png'),
 };
 
 export default icons;
